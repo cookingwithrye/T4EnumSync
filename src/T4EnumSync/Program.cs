38b2c9cf41b86bb51604d02b48a3e192748e5040
Ryan Weir <ryweir@gmail.com>
2013-09-20 00:12:56 -0400

    outlined a fake scenario where this is useful
    Extended the scenario to be a more useful sanity check at runtime

--- src/T4EnumSync/Program.cs
+++ src/T4EnumSync/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace T4EnumSync
 {
@@ -9,6 +10,11 @@ namespace T4EnumSync
     {
         static void Main(string[] args)
         {
+            //our business logic will be a validator to ensure that white motorcycles aren't present in the database. They're kind of lame so we don't want them.
+            
+            //but wait, what if a new color is added and we want special logic for it? Do we update both the enum?
+
+            //we can also have logic to verify at runtime that the enums have corresponding table values in the server as a sanity check
         }
     }
 }
