fa6a5d26c48b5f38d666cd933646d2fd431eee11
Ryan Weir <ryweir@gmail.com>
2013-09-20 00:20:53 -0400

    added vehicle class

---
+++ src/T4EnumSync/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4EnumSync
+{
+    public class Vehicle
+    {
+        public int VehicleID { get; set; }
+
+        public Color VehicleColor { get; set; }
+        public VehicleType Type { get; set; }
+
+        public string Owner { get; set; }
+    }
+}
