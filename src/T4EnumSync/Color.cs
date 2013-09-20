005ca0e8ffbd5824f2f85fabbdb2c83c5adad2e4
Ryan Weir <ryweir@gmail.com>
2013-09-20 00:08:38 -0400

    added enums that are hardcoded

---
+++ src/T4EnumSync/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4EnumSync
+{
+    public enum Color : int
+    {
+        Red = 1,
+        Blue,
+        White
+    }
+
+    public enum VehicleType : int
+    {
+        Truck = 1,
+        Car,
+        MotorCycle
+    }
+}
